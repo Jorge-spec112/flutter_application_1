cad7d3ac1b88bd6eb6d48116f1ea4be2e452326f
Joiner Fernández <jfernandez@akmios.com>
2025-05-21 16:04:02 -0500

    hice la pagina Initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/vistas/Initial.dart';
 import 'package:flutter_application_1/vistas/failure.dart';
 import 'package:flutter_application_1/vistas/loading.dart';
 import 'package:flutter_application_1/vistas/succes.dart';
@@ -10,6 +11,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Failure();
+    return Initial();
   }
 }
---
+++ lib/vistas/Initial.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Initial extends StatelessWidget {
+  const Initial({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(body: Column(children: [Text('Pagina inicial')])),
+    );
+  }
+}
