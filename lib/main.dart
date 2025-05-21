bba9a26f4a8d9b3a019d4c730705618bce1d592e
Joiner Fernández <jfernandez@akmios.com>
2025-05-21 15:10:09 -0500

    cree la vsita succes

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/vistas/succes.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Succes();
+  }
+}
---
+++ lib/vistas/succes.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Succes extends StatelessWidget {
+  const Succes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My app',
+      home: Scaffold(body: Column(children: [Text('pagina inicial')])),
+    );
+  }
+}
