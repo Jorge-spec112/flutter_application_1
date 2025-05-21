9e56709de33218d39713841a9ba0ff5b67ccf22d
Joiner Fernández <jfernandez@akmios.com>
2025-05-21 16:18:21 -0500

    hice el evento home state

--- lib/vistas/bloc/home_state.dart
+++ lib/vistas/bloc/home_state.dart
@@ -2,9 +2,15 @@ part of 'home_bloc.dart';
 
 sealed class HomeState extends Equatable {
   const HomeState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 final class HomeInitial extends HomeState {}
+
+final class HomeLoadInProgress extends HomeState {}
+
+final class HomeLoadSuccess extends HomeState {}
+
+final class HomeLoadFailure extends HomeState {}
