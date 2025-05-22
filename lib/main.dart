913249d148ba5a40c86fee5da821a387c3e1d711
Joiner Fernández <jfernandez@akmios.com>
2025-05-21 21:20:17 -0500

    actualizar el main dart para los widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Personajes.dart';
 import 'package:flutter_application_1/home-page.dart';
+import 'package:flutter_application_1/vistas/Initial.dart';
+import 'package:flutter_application_1/vistas/bloc/home_bloc.dart';
+import 'package:flutter_application_1/vistas/bloc/home_state.dart';
+import 'package:flutter_application_1/vistas/failure.dart';
+import 'package:flutter_application_1/vistas/loading.dart';
 import 'package:flutter_application_1/vistas/succes.dart';
-// O la pantalla de tripulación
-import 'Personajes.dart'; // Donde está la lista
+
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'One Piece App',
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const HomePage(),
-        '/tripulacion': (context) =>
-            SuccessOnePiece(personajes: tripulacionLuffy),
-      },
+    return BlocProvider(
+      create: (context) => HomeBloc(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'My APP',
+        home: BlocBuilder<HomeBloc, HomeState>(
+          builder: (context, state) {
+            if (state is HomeLoadSuccess) {
+              return SuccessOnePiece(personajes: tripulacionLuffy);
+            } else if (state is HomeLoadInProgress) {
+              return Loading();
+            } else if (state is HomeLoadFailure) {
+              return Failure();
+            }
+            return InitialView();
+          },
+        ),
+      ),
     );
   }
 }
--- lib/vistas/succes.dart
+++ lib/vistas/succes.dart
@@ -1,72 +1,100 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/Personajes.dart'; // Asegúrate de importar bien
+import 'package:flutter_application_1/Personajes.dart';
 
-class SuccessOnePiece extends StatelessWidget {
-  const SuccessOnePiece({Key? key, required List<Personaje> personajes})
-    : super(key: key);
+class SuccessOnePiece extends StatefulWidget {
+  final List<Personaje> personajes;
+
+  const SuccessOnePiece({Key? key, required this.personajes}) : super(key: key);
+
+  @override
+  State<SuccessOnePiece> createState() => _SuccessOnePieceState();
+}
+
+class _SuccessOnePieceState extends State<SuccessOnePiece>
+    with SingleTickerProviderStateMixin {
+  double _opacity = 0.4;
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    _controller.repeat(reverse: true);
+    _controller.addListener(() {
+      setState(() {
+        _opacity = 0.4 + (_controller.value * 0.3);
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red[50],
-      appBar: AppBar(
-        title: const Text('Tripulación de Luffy'),
-        backgroundColor: Colors.red[500],
-      ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: tripulacionLuffy.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, // 2 columnas
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-          childAspectRatio: 0.7,
-        ),
-        itemBuilder: (context, index) {
-          final personaje = tripulacionLuffy[index];
-          return Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
+      body: Stack(
+        children: [
+          // Imagen animada de fondo
+          SizedBox.expand(
+            child: AnimatedOpacity(
+              opacity: _opacity,
+              duration: const Duration(seconds: 1),
+              child: Image.asset(
+                'assets/imagenes/sunny.png', // O la imagen que quieras
+                fit: BoxFit.cover,
+              ),
             ),
-            elevation: 4,
-            child: Column(
-              children: [
-                Expanded(
-                  child: ClipRRect(
-                    borderRadius: const BorderRadius.vertical(
-                      top: Radius.circular(12),
-                    ),
-                    child: Image.asset(
-                      personaje.imagen,
-                      fit: BoxFit.cover,
-                      width: double.infinity,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Column(
-                    children: [
-                      Text(
-                        personaje.nombre,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 14,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                      Text(personaje.rol, style: const TextStyle(fontSize: 12)),
-                      Text(
-                        personaje.recompensa,
-                        style: TextStyle(fontSize: 12, color: Colors.grey[700]),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+          ),
+
+          // Contenido de la tripulación
+          GridView.builder(
+            padding: EdgeInsets.all(10),
+            itemCount: widget.personajes.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              childAspectRatio: 0.7,
             ),
-          );
-        },
+            itemBuilder: (context, index) {
+              final p = widget.personajes[index];
+              return Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                elevation: 5,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      p.imagen,
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.contain,
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      p.nombre,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(p.rol),
+                    Text(
+                      p.recompensa,
+                      style: TextStyle(fontSize: 12, color: Colors.grey[700]),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
