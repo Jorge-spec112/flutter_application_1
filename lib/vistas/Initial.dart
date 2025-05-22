729889d2bf8dc4d6633024907d0662c7e0ece451
Joiner Fernández <jfernandez@akmios.com>
2025-05-21 21:09:13 -0500

    actualice el omebloc con el respectivo json que cree en moocy

--- lib/vistas/Initial.dart
+++ lib/vistas/Initial.dart
@@ -22,7 +22,7 @@ class _InitialViewState extends State<InitialView>
     // 👇 Controlador para animación flotante
     _controller = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 2),
+      duration: const Duration(seconds: 5),
     )..repeat(reverse: true);
 
     _animation = Tween<double>(
