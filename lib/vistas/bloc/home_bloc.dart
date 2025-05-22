815164a6576ae9ef77fd6f4fe269eb244b174b7b
Joiner Fernández <jfernandez@akmios.com>
2025-05-21 21:01:41 -0500

    actualizar home event para ponerle homeRequested

--- lib/vistas/bloc/home_bloc.dart
+++ lib/vistas/bloc/home_bloc.dart
@@ -1,5 +1,5 @@
 import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
+
 import 'package:http/http.dart' as http;
 
 import 'home_event.dart';
@@ -9,7 +9,9 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
   HomeBloc() : super(HomeInitial()) {
     on<HomeSearchPressed>((event, emit) async {
       emit(HomeLoadInProgress());
-      final url = Uri.parse('https://jsonplaceholder.typicode.com/posts/1');
+      final url = Uri.parse(
+        'https://run.mocky.io/v3/ca7cde9d-a883-4932-af16-895c856716a4',
+      );
       final response = await http.get(url);
 
       if (response.statusCode == 200) {
